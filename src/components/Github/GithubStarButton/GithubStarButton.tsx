a9cefd8308c10bb009becf6e20ea71708fe40708
sagar gaud <81501729+SAGARGAUD01@users.noreply.github.com>
2023-05-15 11:58:31 +0530

    Files

---
+++ src/components/Github/GithubStarButton/GithubStarButton.tsx
@@ -0,0 +1,20 @@
+const StarButton = () => (
+  <div className="star-button-container">
+    <p>
+      <small>Leave a star on Github if this repository was useful :)</small>
+    </p>
+    <a
+      className="github-button"
+      href="https://github.com/SAGARGAUD01/Shopping-Cart-project"
+      data-icon="octicon-star"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Star SAGARGAUD01/react-shopping-cart on GitHub"
+      tabIndex={-1}
+    >
+      Star
+    </a>
+  </div>
+);
+
+export default StarButton;
---
+++ src/setupTests.ts
@@ -0,0 +1,4 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// expect(element).toHaveTextContent(/react/i)
+// https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
